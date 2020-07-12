23d7d6dd3f5834d5405aab36072afd77305d202b
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-07-11 22:00:40 -0500

    get, post, delete, show open port

--- server.js
+++ server.js
@@ -21,3 +21,65 @@ app.use(express.static(path.join(__dirname, "Develop/public")));
 
 // ROUTER
 
+// GET
+app.get("/api/notes", function(req, res) {
+  
+  noteArr = fs.readFileSync("Develop/db/db.json", "utf8");
+  noteArr = JSON.parse(noteArr);
+  res.json(noteArr);
+});
+
+// POST
+app.post("/api/notes", function(req, res) {
+
+  noteArr = fs.readFileSync("./Develop/db/db.json", "utf8");
+  console.log('Write');
+  console.log(noteArr)
+
+  noteArr = JSON.parse(noteArr);
+  req.body.id = noteArr.length;
+  noteArr.push(req.body);
+  noteArr = JSON.stringify(noteArr);
+ 
+  fs.writeFileSync("./Develop/db/db.json", noteArr, "utf8", function(err) {
+  
+    if (err) {
+      console.log ('error')
+    }
+  });
+  res.json(JSON.parse(noteArr));
+});
+
+// DELETE
+
+app.delete("/api/notes/:id", function(req, res) {
+
+  noteArr = fs.readFileSync("./Develop/db/db.json", "utf8");
+  console.log('Delete')
+ 
+  noteArr = JSON.parse(noteArr);
+
+  noteArr = noteArr.filter(function(note) {
+    return note.id != req.params.id;
+  });
+  
+  noteArr = JSON.stringify(noteArr);
+  
+  fs.writeFileSync("./Develop/db/db.json", noteArr, "utf8", function(err) {
+   
+    if (err) {
+      console.log('error')
+    };
+  });
+
+  res.send(JSON.parse(noteArr));
+});
+
+
+
+
+
+// Start the server on the port
+app.listen(PORT, function() {
+console.log("App listening on PORT: " + PORT)
+})
